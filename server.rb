8ee136c615c51bfdbb930798bc2212547e2b2509
Brady Turner <bturner@nuvotechnologies.com>
2013-02-20 17:26:02 -0500

    client and server handshake, incorrectly though. server loops & returns JSON until connection closed

--- server.rb
+++ server.rb
@@ -33,10 +33,19 @@ EOF
 server = TCPServer.new 2000 # Server bind to port 2000
 loop do
   client = server.accept    # Wait for a client to connect
+  
+  # initial handshake
   client.puts "Protocol: Tesla JSON 1.0"
   client.gets
-  puts "Recv. #2"
-  client.puts JSON.parse(response).to_json
+  client.puts "Channel-Timeout: 300"
+  #client.puts ""
+  # end of handshake
+
+  while !client.closed?
+    client.gets
+    client.puts JSON.parse(response).to_json
+  end
+
   puts "Closing client"
   client.close
 end
