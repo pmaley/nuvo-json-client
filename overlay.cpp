8e5366acc33c273ea43c995e53d72a1adfd88581
Brady Turner <bradyt@nuvotechnologies.com>
2013-03-15 16:05:04 -0400

    added text to connecting overlay

--- overlay.cpp
+++ overlay.cpp
@@ -1,6 +1,8 @@
 #include <QPainter>
+#include <QtWidgets>
 #include <QPen>
 #include <QPalette>
+#include <QLabel>
 #include "overlay.h"
 
 Overlay::Overlay(QWidget *parent)
@@ -15,5 +17,13 @@ void Overlay::paintEvent(QPaintEvent *event)
     painter.setRenderHint(QPainter::Antialiasing);
     QRect r(this->rect());
     painter.fillRect(r,Qt::Dense5Pattern);
+
+    QLabel *waiting = new QLabel("Connecting to system...");
+
+    QGridLayout *l = new QGridLayout();
+
+    l->addWidget(waiting,0,0,Qt::AlignCenter);
+
+    this->setLayout(l);
 
 }
