18ae4dc818870a95ef232ae683ca6c3ac2d14bd0
Brady Turner <bradyt@nuvotechnologies.com>
2013-03-18 12:14:49 -0400

    in ruby client: childInserted, childRemoved

--- nuvoapiclient.rb
+++ nuvoapiclient.rb
@@ -24,19 +24,62 @@ def send_request(request)
   puts @requests.inspect
   @request_num += 1
   @socket.write "#{request.to_json}\n"
+  return @request_num-1
+end
+
+def childInserted(channel,mesg)
+  puts "INSIDE CHILD INSERTED"
+  puts "Insert at index=#{mesg["index"]}"
+  puts @channels[channel]["children"].length
+  @channels[channel]["children"].insert mesg["index"], mesg["item"]
+  puts @channels[channel]["children"].length
+end
+
+def childRemoved(channel,mesg)
+  puts "INSIDE CHILD REMOVED"
+  puts "Remove at index=#{mesg["index"]}"
+  puts @channels[channel]["children"].length
+  @channels[channel]["children"].delete_at mesg["index"]
+  puts @channels[channel]["children"].length
+end
+
+def childValueChanged(channel,mesg)
+  puts "INSIDE CHILD VALUE CHANGED"
+end
+
+def childItemChanged(channel,mesg)
+  puts "INSIDE CHILD ITEM CHANGED"
+end
+
+def parse_closed_message(mesg)
+  puts "CLOSE MESSAGE"
+end
+
+def parse_reply_message(mesg)
+  @requests.delete(mesg["id"])
+  puts @requests.inspect
+  @channels[mesg["channel"]] = mesg["result"]
+end
+
+def parse_event_message(mesg)
+  mesg["event"].each do |event|
+    puts event["type"]
+    method = "#{event["type"]}"
+    send "#{event["type"]}", mesg["channel"], event
+  end
 end
 
 def process_message(mesg)
   puts "*********************************************************"
-  puts "#{mesg["id"]}: #{mesg["channel"]}"
-  @requests.delete(mesg["id"])
-  puts @requests.inspect
-  puts mesg.keys
+  puts "#{mesg["type"]}: #{mesg["id"]}: #{mesg["channel"]}"
+
+  method = "parse_#{mesg["type"]}_message"
+  send method, mesg
 end
 
-send_request(REQ_STABLE_AV)
-send_request(REQ_STABLE_AVS)
-send_request(REQ_STABLE_MUSIC)
+av_req = send_request(REQ_STABLE_AV)
+avs_req = send_request(REQ_STABLE_AVS)
+music_req = send_request(REQ_STABLE_MUSIC)
 
 stdin_buffer = ""
 socket_buffer = ""
