ff7ed3145225d97ce39f94b1f1ea1b8887d976e2
Brady Turner <bradyt@nuvotechnologies.com>
2013-03-19 13:55:23 -0400

    overlay is now solid, transparent blue screen

--- overlay.cpp
+++ overlay.cpp
@@ -15,14 +15,23 @@ void Overlay::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
+    painter.setOpacity(0.7);
+
+    QBrush brush;
+    brush.setColor(Qt::blue);
+    brush.setStyle(Qt::SolidPattern);
+    painter.setBrush(brush);
+
     QRect r(this->rect());
-    painter.fillRect(r,Qt::Dense5Pattern);
+    painter.fillRect(r,brush);
 
-    QLabel *waiting = new QLabel("Connecting to system...");
-    QGridLayout *l = new QGridLayout();
-    l->addWidget(waiting,0,0,Qt::AlignCenter);
 
-    if (!this->layout())
+
+    if (!this->layout()){
+        QLabel *waiting = new QLabel(tr("<font color=\"White\"><b>%1</b></font>").arg("Connecting to system..."));
+        QGridLayout *l = new QGridLayout();
+        l->addWidget(waiting,0,0,Qt::AlignCenter);
         this->setLayout(l);
+    }
 
 }
